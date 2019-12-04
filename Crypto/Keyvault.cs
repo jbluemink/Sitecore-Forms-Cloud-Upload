ed6597d68cfdf1bcb9e0747fe5f1e0ffdd95f906
Jan Bluemink <jan@mirabeau.nl>
2019-12-04 20:48:07 +0100

    cleanup

--- Crypto/Keyvault.cs
+++ Crypto/Keyvault.cs
@@ -2,15 +2,10 @@
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using System.Threading.Tasks;
 
-
 namespace Stockpick.Form.Cloud.Crypto
 {
     public class Keyvault
     {
-        public const int SALT_SIZE = 24; // size in bytes
-        public const int HASH_SIZE = 24; // size in bytes
-        public const int ITERATIONS = 100100; // number of pbkdf2 iterations
-
         public async static Task<string> GetToken(string authority, string resource, string scope)
         {
             var username = Sitecore.Configuration.Settings.GetSetting("Stockpick.Forms.KeyFault.ClientId");
